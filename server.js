066399d4f022f283857f8ad5386dea57dca71b5e
Ferushen <fbaka@list.ru>
2022-03-30 18:25:22 +0300

    feat: add express

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('./dist'))
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/dist/login.html'));
+
+app.listen(port, () => console.log(`App listening to port ${port}`));
